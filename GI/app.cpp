0cbb8a6ca250f2a4fc33416b45b177c0c64b09f1
Makc11m <bubnovmbl2013@yandex.ru>
2026-03-16 22:09:43 +0300

    Fragment Interpolation

--- GI/app.cpp
+++ GI/app.cpp
@@ -24,9 +24,9 @@ void Application::run() {
 
 void Application::loadModels() {
     std::vector<Model::Vertex> vertices{
-        {{0.0f, 0.5f}},
-        {{0.5f, 0.5f}},
-        {{-0.5f, 0.5f}}
+        {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+        {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+        {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
     };
 
     model = std::make_unique<Model>(device, vertices);
