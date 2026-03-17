8af8002f47f8f3fdc738d9d40ccc09326783142c
Makc11m <bubnovmbl2013@yandex.ru>
2026-03-17 16:06:03 +0300

    Tranformation

---
+++ GI/simple_render_system.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "device.hpp"
+#include "pipeline.hpp"
+#include "game_object.hpp"
+
+// std
+#include <memory>
+#include <vector>
+
+class SimpleRenderSystem {
+public:
+	SimpleRenderSystem(Device& device, VkRenderPass renderPass);
+	~SimpleRenderSystem();
+
+	SimpleRenderSystem(const SimpleRenderSystem&) = delete;
+	SimpleRenderSystem& operator=(const SimpleRenderSystem&) = delete;
+
+	void renderGameObjects(VkCommandBuffer commandBuffer, std::vector<GameObject>& gameObjects);
+
+private:
+	void createPipelineLayout();
+	void createPipeline(VkRenderPass renderPass);
+
+	Device& device;
+
+
+	std::unique_ptr<Pipeline> pipeline;
+	VkPipelineLayout pipelineLayout;
+};
