d138777523978a0ebbcfb12e19b14b6d0de0efb7
Makc11m <bubnovmbl2013@yandex.ru>
2026-03-17 17:09:43 +0300

    Revert "Clear"
    This reverts commit d983148cd80c65fcb01afbb1166a430694ac753a.

---
+++ GI/app.cpp
@@ -0,0 +1,52 @@
+#include "app.hpp"
+
+#include "simple_render_system.hpp"
+
+// std
+#include <iostream>
+#include <array>
+#include <stdexcept>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+
+Application::Application() { loadGameObjects(); }
+
+Application::~Application() {}
+
+void Application::run() {
+	SimpleRenderSystem simpleRenderSystem{ device, renderer.getSwapChainRenderPass() };
+
+    while (!window.shouldClose()) {
+        glfwPollEvents();
+
+		if (auto commandBuffer = renderer.beginFrame()) {
+            renderer.beginSwapChainRenderPass(commandBuffer);
+            simpleRenderSystem.renderGameObjects(commandBuffer, gameObjects);
+            renderer.endSwapChainRenderPass(commandBuffer);
+            renderer.endFrame();
+        }
+    }
+
+    vkDeviceWaitIdle(device.device());
+}
+
+void Application::loadGameObjects() {
+    std::vector<Model::Vertex> vertices{
+        {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+        {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+        {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
+    };
+    auto model = std::make_shared<Model>(device, vertices);
+
+	auto triangle = GameObject::createGameObject();
+	triangle.model = model;
+    triangle.color = {0.1f, 0.8f, 0.1f};
+	triangle.transform2d.translation.x = 0.2f;
+    triangle.transform2d.scale = {2.0f, 0.5f};
+    triangle.transform2d.rotation = 0.25f * glm::two_pi<float>();
+
+    gameObjects.push_back(std::move(triangle));
+}
